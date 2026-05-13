5af6334db13584a5ddf6a251f14acc46bdb043f0
jbeautymua <jssbeautymua@gmail.com>
2026-05-14 00:00:01 +0530

    feat(analytics): integrate Google Analytics using @next/third-parties

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Inter, Cormorant_Garamond } from "next/font/google";
 import "./globals.css";
 import { StructuredData } from "@/components/StructuredData";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({
   subsets: ["latin"],
@@ -63,6 +64,7 @@ export default function RootLayout({
       <body className="antialiased">
         <StructuredData />
         {children}
+        <GoogleAnalytics gaId="G-YX0VTMCRHD" />
       </body>
     </html>
   );
