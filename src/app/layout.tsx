19736cabb2886c41657c407954fa2af09f02989c
gouthamksuresh <me.goutham.tech@gmail.com>
2026-06-25 01:39:22 +0530

    chore: add microsoft clarity script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,6 +77,15 @@ export default function RootLayout({
           strategy="afterInteractive"
           crossOrigin="anonymous"
         />
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wsxn2nboqa");
+          `}
+        </Script>
       </body>
     </html>
   );
